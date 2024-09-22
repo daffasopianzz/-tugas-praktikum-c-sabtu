574a298cd22da0a6d6054cdc414afddf637c1717
DaffaSopian <dafasopian56@gmail.com>
2024-09-22 12:29:56 +0700

    [fix] keyword

--- tugas_kedua.c
+++ tugas_kedua.c
@@ -4,6 +4,6 @@ int main() {
     int panjang = 10;
     int lebar = 7;
     int luas = panjang * lebar;
-    printf("Luas persegi panjang: %d cm\n", luas);
+    printf("Luas persegi panjang: %d cm^2\n", luas);
     return 0;
 }
